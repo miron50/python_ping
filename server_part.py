d79ec1ee2ac9c173cfbfa4ce7c0d3c22cbd3964c
miron50 <38781962+miron50@users.noreply.github.com>
2018-04-27 13:22:12 +0300

    Add files via upload

--- server_part.py
+++ server_part.py
@@ -6,30 +6,30 @@ if __name__ == "__main__":
     recv = 1024
     local_port = "2222"
 
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_sock.bind(("localhost", int(local_port)))
-    server_sock.listen()
-    list_connections.append(server_sock)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_sock.bind(("localhost", int(local_port)))
+        server_sock.listen()
+        list_connections.append(server_sock)
 
-    print("Server is started and listen port " + str(local_port))
+        print("Server is started and listen port " + str(local_port))
 
-    while 1:
-        read_sockets, write_sockets, error_sockets = select.select(list_connections, [], [])
+        while 1:
+            read_sockets, write_sockets, error_sockets = select.select(list_connections, [], [])
 
-        for sock in read_sockets:
-            if sock == server_sock:
-                sockfd, client_ip_port = server_sock.accept()
-                list_connections.append(sockfd)
-                print("Client (%s, %s) connected to server" % client_ip_port)
-            else:
-                try:
-                    data = sock.recv(recv)
-                    if data:
-                        sock.send(data)
-                    else:
-                        raise ConnectionResetError()
-                except:
-                    print("Client (%s, %s) is disconnected" % client_ip_port)
-                    sock.close()
-                    list_connections.remove(sock)
+            for sock in read_sockets:
+                if sock == server_sock:
+                    sockfd, client_ip_port = server_sock.accept()
+                    list_connections.append(sockfd)
+                    print("Client (%s, %s) connected to server" % client_ip_port)
+                else:
+                    try:
+                        data = sock.recv(recv)
+                        if data:
+                            sock.send(data)
+                        else:
+                            raise ConnectionResetError()
+                    except:
+                        print("Client (%s, %s) is disconnected" % client_ip_port)
+                        sock.close()
+                        list_connections.remove(sock)
